f76072d5dee5034427adbfde92d0cdbe9ceb14da
Darko <darkogligorijevic4@gmail.com>
2023-08-16 11:33:53 +0200

    Work on confirm password and add success file

--- script.js
+++ script.js
@@ -1,6 +1,12 @@
 const form = document.querySelector(".form-container");
+const password = document.querySelector(".password");
+const passwordConfirm = document.querySelector('.password-confirm');
+const error = document.querySelector('.error');
+
 
 let prevClickedSpan;
+let passwordUser = '';
+let confirmedPasswordUser = '';
 
 
 form.addEventListener('click', (e) => {
@@ -8,7 +14,10 @@ form.addEventListener('click', (e) => {
   let clickedInput;
   clickedSpan = e.target.closest('span');
   clickedInput = e.target.closest('input');
-  if(!clickedInput && !clickedSpan) return;
+  if(!clickedInput && !clickedSpan) {
+    if(prevClickedSpan) prevClickedSpan.classList.remove('clicked');
+    return;
+  }
   console.log('clicked');
   if(clickedInput) {
     clickedSpan = clickedInput.nextElementSibling ;
@@ -20,3 +29,45 @@ form.addEventListener('click', (e) => {
   clickedSpan.classList.add('clicked');
   prevClickedSpan = clickedSpan;
 })
+
+password.addEventListener('keypress', (e) => {
+  passwordUser += e.key;
+  console.log(passwordUser);
+  if(passwordUser.match("^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)[a-zA-Z\d]{8,}$")) {
+    password.classList.remove('password')
+  }
+})
+
+password.addEventListener('keydown', (e) => {
+  if(e.key === 'Backspace' && passwordUser.length > 0) {
+    passwordUser = passwordUser.slice(0,-1);
+  }
+
+})
+
+passwordConfirm.addEventListener('keypress', (e) => {
+  confirmedPasswordUser += e.key
+  if(confirmedPasswordUser === passwordUser) {
+    error.textContent = "";
+    passwordConfirm.classList.remove('password-confirm');
+  } else {
+    error.textContent = "*Password did not match!" 
+  }
+  console.log(e.key);
+})
+
+passwordConfirm.addEventListener('keydown', (e) => {
+  if(e.key === 'Backspace' && confirmedPasswordUser.length > 0) {
+    confirmedPasswordUser = confirmedPasswordUser.slice(0,-1);
+  }
+
+})
+
+
+// formm.addEventListener('submit', (e) => {
+//   e.preventDefault();
+//   const inputs = document.querySelectorAll('inputs');
+//   inputs.forEach(input=> {
+//     console.log(input);
+//   })
+// })
